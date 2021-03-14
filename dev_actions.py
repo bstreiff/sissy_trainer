558076b5399a406147aee8ba8e8c0622b69439eb
bstreiff <git@streiff.net>
2021-03-14 14:24:11 -0500

    dev_actions: add pre-commit hook

--- dev_actions.py
+++ dev_actions.py
@@ -3,6 +3,7 @@
 
 import subprocess
 import os
+import stat
 
 _MODULE = "sistrum"
 
@@ -15,3 +16,32 @@ def check():
     # Code coverage
     subprocess.run(["coverage", "run", "-m", "--source=" + _MODULE, "pytest"], cwd=basedir)
     subprocess.run(["coverage", "html"], cwd=basedir)
+
+
+def pre_commit_hook():
+    check()
+
+
+def install_hooks():
+    basedir = os.path.dirname(os.path.realpath(__file__))
+
+    pre_commit_hook_str = r'''
+#!/bin/sh
+
+POETRY=$(which poetry)
+if [ "$POETRY" = "" ]; then
+	echo "*** Can't find poetry to invoke pre-commit hook!"
+fi
+if [ -e "$POETRY.bat" ]; then
+	# On Windows, we want to run the .bat file sitting alongside.
+	POETRY="$POETRY.bat"
+fi
+
+exec $POETRY run pre_commit_hook
+'''.lstrip('\n')
+
+    pre_commit_path = os.path.join(basedir, ".git", "hooks", "pre-commit")
+
+    with open(pre_commit_path, "w") as fd:
+        fd.write(pre_commit_hook_str)
+    os.chmod(pre_commit_path, stat.S_IRWXU | stat.S_IRGRP | stat.S_IROTH)
